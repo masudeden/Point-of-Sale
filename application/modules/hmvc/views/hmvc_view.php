611755cc2e7bf1880f95e96a0729fcb6b310ae0c
JaCk <jibigopi007@gmail.com>
2013-06-25 08:55:16 +0530

    delete-css-php-and-js-files

--- application/modules/hmvc/views/hmvc_view.php
+++
@@ -1,1 +0,0 @@
-<h2>This is from the HMVC module page.</h2>
--- application/views/brands/add_brand.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo form_open('brands/add_brands');
-echo "<table>";
-echo "<tr><td>";echo form_label($this->lang->line('brands_name'));echo "</td><td>"; echo form_input('name',set_value('name'),'id=name autofocus');echo "</td></tr>";
-echo "<tr><td>";echo form_submit('save',$this->lang->line('save'));echo "</td><td>";echo form_submit('cancel',$this->lang->line('cancel'));echo "</td></tr>";
-echo form_close();
-echo "</table>";
-echo validation_errors();
-?>
