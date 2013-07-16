0c58362b1aa0d0db1ec8a068188c19b27f224809
JaCk <jibigopi007@gmail.com>
2013-07-16 23:59:06 +0530

    purchase order module

---
+++ application/modules/purchase_oder/language/english/english_lang.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['tax']='Tax';;
+$lang['save']='Save';
+$lang['cancel']='Cancel';
+$lang['update']='Update';
+$lang['add']='Add';
+$lang['delete']='Delete';
+$lang['add_new_supplier']='Add New Supplier';
+$lang['back_to_home']='Back To Home';
+$lang['edit']='Edit';
+$lang['edit_permission']='Edit Permission';
+$lang['first_name']='First Name';
+$lang['last_name']='Last Name';
+$lang['sex']='Sex';
+$lang['age']='Age';
+$lang['address']='Address';
+$lang['address1']='Address 1';
+$lang['address2']='Address 2';
+$lang['city']='City';
+$lang['state']='State';
+$lang['zip']='Zip ';
+$lang['country']='Country';
+$lang['email']='Email ';
+$lang['phone']='Phone ';
+$lang['date_of']='Date OF Birth';
+$lang['company']='Comapany';
+$lang['website']='Website';
+$lang['comments']='Comments';
+$lang['account_no']='Account Number';
+$lang['active']='Active';
+$lang['deactive']='Deactive';
+$lang['restore']='Restore';
+$lang['suppler_company']='Company :';
+$lang['name']='Name';
+$lang['addnewporder']='Add Purchse Order';
+ $lang['supplier code']='Company Name';
+ $lang['supplier name']='Supplier Name';
+ $lang['exp_date']='Expiry Date[dd/mm/yyyy]';
+ $lang['pono']='PO No';
+ $lang['podate']='PO Date';
+ $lang['discount']='Discount';
+ $lang['disamount']='Discount Amount';
+ $lang['Freight']='Freight';
+ $lang['Round off Amount']='Round off Amount';
+
+
+
+
+?>﻿
