74e1f70bb41fbbebac4b1ef43e4f7f48813ef22c
jibigopi <jibigopi007@gmail.com>
2014-03-27 17:52:59 +0530

    Item Setting

--- application/modules/items_setting/controllers/items_setting.php
+++ application/modules/items_setting/controllers/items_setting.php
@@ -4,14 +4,11 @@ class Items_setting extends CI_Controller{
                 parent::__construct();
                $this->load->library('posnic');   
     }
-    function index(){     
-         if(!isset($_SESSION['Uid'])){
-                redirect('home');
-        }else{
+    function index(){ 
             $this->get_items();
-        }
     }
-     function get_items(){                  
+    
+    function get_items(){                  
         $this->load->view('template/app/header'); 
         $this->load->view('header/header');         
         $this->load->view('template/branch',$this->posnic->branches());
@@ -20,89 +17,80 @@ class Items_setting extends CI_Controller{
         $this->load->view('template/app/navigation',$this->posnic->modules());
         $this->load->view('template/app/footer');
     }
-        function data_table(){
-        $aColumns = array( 'name', 'code','name','location','b_name','c_name','guid','active','guid' );	
-	$start = "";
-			$end="";
-		
-		if ( $this->input->get_post('iDisplayLength') != '-1' )	{
-			$start = $this->input->get_post('iDisplayStart');
-			$end=	 $this->input->get_post('iDisplayLength');              
-		}	
-		$order="";
-		if ( isset( $_GET['iSortCol_0'] ) )
-		{	
-			for ( $i=0 ; $i<intval($this->input->get_post('iSortingCols') ) ; $i++ )
-			{
-				if ( $_GET[ 'bSortable_'.intval($this->input->get_post('iSortCol_'.$i)) ] == "true" )
-				{
-					$order.= $aColumns[ intval( $this->input->get_post('iSortCol_'.$i) ) ]." ".$this->input->get_post('sSortDir_'.$i ) .",";
-				}
-			}
-			
-					$order = substr_replace( $order, "", -1 );
-					
-		}
-		
-		$like = array();
-		
-			if ( $_GET['sSearch'] != "" )
-		{
-		$like =array('upc_ean_code'=>  $this->input->get_post('sSearch'),'items.name'=>  $this->input->get_post('sSearch'),'code'=>  $this->input->get_post('sSearch'));
-				
-			}
-			$this->load->model('core_model')		   ;
-			 $rResult1 = $this->core_model->items_data_table($end,$start,$order,$like,$_SESSION['Bid']);
-		   
-		$iFilteredTotal =$this->posnic->data_table_count('items');
-		
-		$iTotal =$this->posnic->data_table_count('items');
-		
-		$output1 = array(
-			"sEcho" => intval($_GET['sEcho']),
-			"iTotalRecords" => $iTotal,
-			"iTotalDisplayRecords" => $iFilteredTotal,
-			"aaData" => array()
-		);
-		foreach ($rResult1 as $aRow )
-		{
-			$row = array();
-			for ( $i=0 ; $i<count($aColumns) ; $i++ )
-			{
-				if ( $aColumns[$i] == "id" )
-				{
-					$row[] = ($aRow[ $aColumns[$i] ]=="0") ? '-' : $aRow[ $aColumns[$i] ];
-				}
-				else if ( $aColumns[$i] != ' ' )
-				{
-					/* General output */
-					$row[] = $aRow[$aColumns[$i]];
-				}
-				
-			}
-				
-		$output1['aaData'][] = $row;
-		}
-                
-		
-		   echo json_encode($output1);
+    
+    function data_table(){
+        $aColumns = array( 'name', 'code','name','location','b_name','c_name','guid','active_status','guid' );	
+        $start = "";
+        $end="";
+        if ( $this->input->get_post('iDisplayLength') != '-1' )	{
+            $start = $this->input->get_post('iDisplayStart');
+            $end=	 $this->input->get_post('iDisplayLength');              
+        }	
+        $order="";
+        if ( isset( $_GET['iSortCol_0'] ) )
+            {	
+                for ( $i=0 ; $i<intval($this->input->get_post('iSortingCols') ) ; $i++ )
+                    {
+                        if ( $_GET[ 'bSortable_'.intval($this->input->get_post('iSortCol_'.$i)) ] == "true" )
+                        {
+                            $order.= $aColumns[ intval( $this->input->get_post('iSortCol_'.$i) ) ]." ".$this->input->get_post('sSortDir_'.$i ) .",";
+                        }
+                    }
+                $order = substr_replace( $order, "", -1 );
+            }
+        $like = array();
+        if ( $_GET['sSearch'] != "" )
+            {
+                $like =array('upc_ean_code'=>  $this->input->get_post('sSearch'),'items.name'=>  $this->input->get_post('sSearch'),'code'=>  $this->input->get_post('sSearch'));
+            }
+        $this->load->model('core_model')		   ;
+        $rResult1 = $this->core_model->items_data_table($end,$start,$order,$like,$_SESSION['Bid']);
+        $iFilteredTotal =$this->posnic->data_table_count('items');
+        $iTotal =$this->posnic->data_table_count('items');
+        $output1 = array(
+            "sEcho" => intval($_GET['sEcho']),
+            "iTotalRecords" => $iTotal,
+            "iTotalDisplayRecords" => $iFilteredTotal,
+            "aaData" => array()
+        );
+        foreach ($rResult1 as $aRow )
+        {
+            $row = array();
+            for ( $i=0 ; $i<count($aColumns) ; $i++ )
+            {
+                if ( $aColumns[$i] == "id" )
+                    {
+                        $row[] = ($aRow[ $aColumns[$i] ]=="0") ? '-' : $aRow[ $aColumns[$i] ];
+                    }
+                    else if ( $aColumns[$i] != ' ' )
+                    {
+                        $row[] = $aRow[$aColumns[$i]];
+                    }
+            }
+
+            $output1['aaData'][] = $row;
+        }
+        echo json_encode($output1);
     }
+    
     function get_items_setting_details($guid){
         $this->load->model('core_model');
         $data[0]=  $this->core_model->get_items_details_for_update($_SESSION['Bid'],$guid);
         $data[1]=  $this->core_model->get_items_setting_details($_SESSION['Bid'],$guid);
-        echo json_encode($data);
-        
+        echo json_encode($data);       
     }
+    
     function set_item($guid){
         $data['guid']=$guid;
         $this->load->view('set_item',$data);
     }
+    
     function reset_item($guid){
         $where=array('guid'=>$guid);
         $data['row']=$this->posnic->posnic_result($where);
         $this->load->view('edit_setting',$data);
     }
+    
     function set_items_setting(){
         if($_SESSION['items_setting_per']['set']==1){
             $guid=$this->input->post('guid');
@@ -122,70 +110,15 @@ class Items_setting extends CI_Controller{
                     $where=array('guid'=>$guid);
                     $this->posnic->posnic_module_update('items_setting',$data,$where);
                     echo 'TRUE';
-                   // redirect('items_setting'); 
-            }else{
-              //  $this->set_item($guid);
+                  
+            }else{  echo 'FALSE';
             }
-        }else{
-           // redirect('items_setting');
+        }else{ 
+            echo 'Noop';
         }   
         
     }
-      
-    function items_details(){
-       
-        
-             if($this->input->post('cancel')){
-                redirect('home');
-            }
-             if($this->input->post('edit')){
-                  $data['row'] = $this->input->post('posnic'); 
-                            if(!$data['row']==''){   
-                            
-                            $this->load->view('bulk_edit_setting',$data);
-                            
-                            }else{
-                                redirect('items_setting');
-                            }
-             }
-        
-        
-    }
-    function bult_update(){        
-        if (!$_SERVER['HTTP_REFERER']){ redirect('home');}
-        else{
-            if($this->input->post('cancel')){
-                redirect('items_setting');
-            }
-            if($this->input->post('save')){
-             $data=$this->input->post('guid');
-              $this->load->library('form_validation');
-               $this->form_validation->set_rules("min_qty",$this->lang->line('min_qty'),'required|max_length[15]|regex_match[/^[0-9]+$/]|xss_clean');                                             
-               $this->form_validation->set_rules("max_qty",$this->lang->line('max_qty'),'required|max_length[15]|regex_match[/^[0-9]+$/]|xss_clean');                           
-            if ($this->form_validation->run() !== false ) {       
-                     $data1=array(
-                    'set'=>1,
-                    'sales'=>$this->input->post('sale')?1:0,
-                    'salses_return'=>$this->input->post('salses_return')?1:0,
-                    'purchase'=>$this->input->post('purchase')?1:0,
-                    'purchase_return'=>$this->input->post('purchase_return')?1:0,
-                    'allow_negative'=>$this->input->post('allow_negative')?1:0,
-                    'tax_inclusive'=>$this->input->post('tax'),
-                    'min_q'=>$this->input->post('min_qty'),
-                    'max_q'=>$this->input->post('max_qty'));
-                     foreach( $data as $key => $value){ 
-                         $where=array('guid'=>$value);
-                         $this->posnic->posnic_update($data1,$where);
-                       } 
-                redirect('items_setting'); 
-            }else{
-                           
-                            $this->load->view('bulk_edit_setting',$data);
-                            
-            }
-            }
-        }
-    }
+    
     
 }
 ?>
