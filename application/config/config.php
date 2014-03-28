015c82db4f3e34f13602f8716f0117f0185f56a1
Jibi gopi <jibigopi007@gmail.com>
2014-03-28 20:48:26 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/Point-of-Sale/';
+$config['base_url']	= 'http://localhost/posnic/';
 
 /*
 |--------------------------------------------------------------------------
