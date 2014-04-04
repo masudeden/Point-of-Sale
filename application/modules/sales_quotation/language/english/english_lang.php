4bf66538e737af9eb117ccb7261ba75d7463ae83
jibigopi <jibigopi007@gmail.com>
2014-04-05 02:01:33 +0530

    sales quotation

--- application/modules/sales_quotation/language/english/english_lang.php
+++ application/modules/sales_quotation/language/english/english_lang.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 $lang['quotation_date']='Quotation Date';
 $lang['total_item_discount_amount']='Item Disc Amount';
-$lang['total_tax']='Total Tax';
+$lang['total_tax']='Total  Exclusive Tax';
 $lang['tax_type']='Tax Type';
 
 
